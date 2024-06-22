0a6d937eec40a19f259febb859e840e907eced40
Mohammadrezaheidariezzati <mohammadrezaheidariezzati7408@gmail.com>
2024-06-22 19:46:29 +0330

    .

--- scripts/papers.js
+++ scripts/papers.js
@@ -11,27 +11,3 @@ toggleBtn.onclick = function(){
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
 }
-
-/*popup images*/
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
-
-document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () =>{
-    preveiwContainer.style.display = 'flex';
-    let name = product.getAttribute('data-name');
-    previewBox.forEach(preview =>{
-      let target = preview.getAttribute('data-target');
-      if(name == target){
-        preview.classList.add('active');
-      }
-    });
-  };
-});
-
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
-    close.classList.remove('active');
-    preveiwContainer.style.display = 'none';
-  };
-});
